c48d35355b1982c114f7fe0a0f5c420981602064
Erik Olsen <erik.s.olsen@gmail.com>
2018-09-21 20:36:52 -0500

    Adds some basic validation around valid knight moves

--- src/components/Knight.js
+++ src/components/Knight.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Draggable from 'react-draggable'
 import { API_ROOT, HEADERS, SQUARE_SIZE } from '../constants'
+const canMoveKnight = (toX, toY) => {
+  return (Math.abs(toX) === 100 && Math.abs(toY) === 50) ||
+         (Math.abs(toX) === 50 && Math.abs(toY) === 100);
+}
 
 const Knight = ({children, white, black, position, row, col})=>{
   let colors = white ? ' text-white border-white bg-orange-darker' :
@@ -9,7 +13,7 @@ const Knight = ({children, white, black, position, row, col})=>{
   let classes = "w-4 fixed border-2 rounded-full" + colors
   return(
     <div>
-      <Draggable onStop={(e,ui)=>{ moveKnight(ui, white, position, row, col)}} grid={[SQUARE_SIZE, SQUARE_SIZE]} >
+      <Draggable onStop={(e,ui)=>{ moveKnight(ui, position, row, col)}} grid={[SQUARE_SIZE, SQUARE_SIZE]} >
         <div style={{width: SQUARE_SIZE+'px'}} className={classes}>&#9822;</div>
       </Draggable>
       <div>{children}</div>
@@ -17,19 +21,22 @@ const Knight = ({children, white, black, position, row, col})=>{
   )
 }
 
-const moveKnight = (ui, white, position, row, col)=> {
+const moveKnight = ( ui, position, row, col)=> {
   col = (ui.lastX / SQUARE_SIZE) + col
   row = (ui.lastY / SQUARE_SIZE) + row
+  let canMove = canMoveKnight(ui.lastX, ui.lastY)
+
   let gameId = window.location.pathname.split('/').last()
 
   let square = [row, col]
-  if (row >= 0 && col >= 0){
+  if (canMove){
     fetch(`${API_ROOT}/games/${gameId}/move`, {
       method: 'POST',
       headers: HEADERS,
       body: JSON.stringify({'game': {'position' : position, 'move': square} } )
     });
   } else {
+    window.location.reload()
     console.log('ERROR')
   }
 }
