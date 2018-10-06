791824ced68826b1935308d3a39fc4162534a899
Erik Olsen <erik.s.olsen@gmail.com>
2018-10-05 22:49:39 -0500

    Calculate hand value when both knights are on same row

--- src/HandCalculator.js
+++ src/HandCalculator.js
@@ -25,8 +25,7 @@ export default class HandCalculator {
     _.replace(card, '♥', 'h');
     _.replace(card, '♣', 'c');
     _.replace(card, '♦', 'd');
-    _.replace(card, '10', 'T');
-    return card
+    return _.replace(card, '10', 'T')
   }
 
   whiteWins(){
@@ -44,21 +43,31 @@ export default class HandCalculator {
   whiteHands(){
     let playerOneRows = this.knights.slice(0,2).map((x)=> x[0])
 
-    let whiteOneA = Hand.solve(_.concat(this.cards[playerOneRows[0]], this.white[0]).map((card)=> this.swapCard(card)))
-    let whiteOneB = Hand.solve(_.concat(this.cards[playerOneRows[0]], this.white[1]).map((card)=> this.swapCard(card)))
-    let whiteTwoA = Hand.solve(_.concat(this.cards[playerOneRows[1]], this.white[0]).map((card)=> this.swapCard(card)))
-    let whiteTwoB = Hand.solve(_.concat(this.cards[playerOneRows[1]], this.white[1]).map((card)=> this.swapCard(card)))
-    return Hand.winners([whiteOneA, whiteOneB, whiteTwoA, whiteTwoB])
+    if(playerOneRows[0] === playerOneRows[1]){
+      let hand = Hand.solve(_.concat(this.cards[playerOneRows[0]], this.white).map((card)=> this.swapCard(card)))
+      return Hand.winners([hand])
+    } else {
+      let whiteOneA = Hand.solve(_.concat(this.cards[playerOneRows[0]], this.white[0]).map((card)=> this.swapCard(card)))
+      let whiteOneB = Hand.solve(_.concat(this.cards[playerOneRows[0]], this.white[1]).map((card)=> this.swapCard(card)))
+      let whiteTwoA = Hand.solve(_.concat(this.cards[playerOneRows[1]], this.white[0]).map((card)=> this.swapCard(card)))
+      let whiteTwoB = Hand.solve(_.concat(this.cards[playerOneRows[1]], this.white[1]).map((card)=> this.swapCard(card)))
+      return Hand.winners([whiteOneA, whiteOneB, whiteTwoA, whiteTwoB])
+    }
   }
 
   blackHands(){
     let playerTwoRows = this.knights.slice(2,4).map((x)=> x[0])
 
-    let blackOneA = Hand.solve(_.concat(this.cards[playerTwoRows[0]], this.black[0]).map((card)=> this.swapCard(card)))
-    let blackOneB = Hand.solve(_.concat(this.cards[playerTwoRows[0]], this.black[1]).map((card)=> this.swapCard(card)))
-    let blackTwoA = Hand.solve(_.concat(this.cards[playerTwoRows[1]], this.black[0]).map((card)=> this.swapCard(card)))
-    let blackTwoB = Hand.solve(_.concat(this.cards[playerTwoRows[1]], this.black[1]).map((card)=> this.swapCard(card)))
-    return Hand.winners([blackOneA, blackOneB, blackTwoA, blackTwoB])
+    if(playerTwoRows[0] === playerTwoRows[1]){
+      let hand = Hand.solve(_.concat(this.cards[playerTwoRows[0]], this.black).map((card)=> this.swapCard(card)))
+      return Hand.winners([hand])
+    } else {
+      let blackOneA = Hand.solve(_.concat(this.cards[playerTwoRows[0]], this.black[0]).map((card)=> this.swapCard(card)))
+      let blackOneB = Hand.solve(_.concat(this.cards[playerTwoRows[0]], this.black[1]).map((card)=> this.swapCard(card)))
+      let blackTwoA = Hand.solve(_.concat(this.cards[playerTwoRows[1]], this.black[0]).map((card)=> this.swapCard(card)))
+      let blackTwoB = Hand.solve(_.concat(this.cards[playerTwoRows[1]], this.black[1]).map((card)=> this.swapCard(card)))
+      return Hand.winners([blackOneA, blackOneB, blackTwoA, blackTwoB])
+    }
   }
 
   winner(){
